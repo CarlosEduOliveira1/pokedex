e30a83f742c3f56a4ef7a9eb3889550ed7e77cc4
Carlos Oliveira <carlosoliveira.cee@gmail.com>
2026-02-05 21:51:59 -0300

    Adding a header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="Header">
+      <a href="/" rel="noopener noreferrer" className="headerLink">
+        <img src="./pokeball.svg" alt="Pokeball" id="logo"/>
+        <h1 id="headerTitle">Pokédex</h1>
+      </a>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import ApiPokemon from './components/ApiPokemon.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import ApiPokemon from './components/ApiPokemon.jsx';
+import Header from './components/Header/Header.jsx';
 //import dotenv from 'dotenv';
 
 //dotenv.config();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    <ApiPokemon />
+    <Header />
+    {/* <App /> */}
+    {/* <ApiPokemon /> */}
   </StrictMode>,
 )
