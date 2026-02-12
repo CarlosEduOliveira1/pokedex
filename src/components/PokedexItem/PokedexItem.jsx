5264abb9fc06c9ff016fe53ac585092b84cf98c1
Carlos Oliveira <carlosoliveira.cee@gmail.com>
2026-02-11 22:04:18 -0300

    Improving the pokedex listing

--- src/components/PokedexItem/PokedexItem.jsx
+++ src/components/PokedexItem/PokedexItem.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "./PokedexItem.css";
 
-const PokedexItem = () => {
+const PokedexItem = (pokemon) => {
   return (
     <div className="pokemon-card">
       <div className="pokemon-id">#132</div>
       <div className="pokemon-image">
-        <img src="./132-official.png" alt="Ditto" />
+        <img src={pokemon.pokemon.sprites.other["official-artwork"].front_default ?? ''} alt="Ditto" />
       </div>
       <div className="pokemon-name">
-        <h2>Ditto</h2>
+        <h2>{pokemon.pokemon.name}</h2>
       </div>
     </div>
   );
